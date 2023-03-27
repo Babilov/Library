544fb8d096a1376fafb3892e351a4f41f2753bdf
Babilov <egor-kun2017@mail.ru>
2023-03-28 00:56:40 +0300

    add model Book

--- main/models.py
+++ main/models.py
@@ -8,4 +8,5 @@ class Book(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
-
+    def __str__(self):
+        return f'{str(self.authors)} {str(self.title)} {str(self.year)}'
