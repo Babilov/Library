04990008caefded2aaa355c8b75f1368640a82ea
Babilov <egor-kun2017@mail.ru>
2023-03-28 00:54:54 +0300

    add model Book

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 
+class Book(models.Model):
+    authors = models.JSONField()
+    title = models.CharField(max_length=250)
+    year = models.IntegerField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
